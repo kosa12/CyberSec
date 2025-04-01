6242133c658776eee2b248cd3f767b31925a106b
kosa12 <kosa03matyas@gmail.com>
2025-04-01 14:29:35 +0300

    Type safety

--- security-testing/demo/demo.py
+++ security-testing/demo/demo.py
@@ -1,22 +1,23 @@
-
+from typing import Tuple
 
 class BankAccount:
     
-    def __init__(self, owner, account_number, initial_balance = 0):
+    def __init__(self, owner: str, account_number: Tuple[int, int, int], initial_balance: int = 0) -> None:
+        
         self.owner = owner
         self.account_number = account_number
         self.balance = initial_balance
     
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> None:
         self.balance += amount
     
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> None:
         self.balance -= amount
     
-    def is_overdrawn(self):
+    def is_overdrawn(self) -> bool:
         return self.balance < 0
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Balance of {self.owner} (account number: {self.account_number}) is: {self.balance}"
 
 
@@ -24,4 +25,4 @@ account = BankAccount("Dagobert Duck", (12345678,87654321,00000000), 15)
 account.withdraw(5)
 print(account)
 
-account.deposit("test")
+# account.deposit("test")
